b20297908adf48bb1304e7026bf67c48fd8b443b
Tarik <tarikkof@gmail.com>
2020-01-24 03:07:27 +0100

    Add trending controller

---
+++ controllers/trending/index.js
@@ -0,0 +1,55 @@
+var trending = require('../../modules/trending');
+
+module.exports = {
+  trendRepos : function(req,res){
+    trending.trendRepos(function(out){
+      if (out) {
+          res.status(200);
+          res.json({
+            status: 200,
+            data : out
+          });
+      } else {
+        res.status(500)
+        res.json({
+            status: 500,
+            message : 'Oops, something unexpected happened. Blame Tarik Moustaid, I\'m just a program.'
+          });
+      }
+    });
+  },
+   langRepos : function(req,res){
+    trending.langRepos(req.query.lang, function(out){
+      if (out) {
+          res.status(200);
+          res.json({
+            status: 200,
+            data : out
+          });
+      } else {
+        res.status(500)
+        res.json({
+            status: 500,
+            message : 'Oops, something unexpected happened. Maybe you didnt specify the language? ?lang= or you need to put the blame on Tarik Moustaid, I\'m just a program.'
+          });
+      }
+    });
+  },
+  langsAnalytics : function(req,res){
+    trending.langsAnalytics(function(out){
+      if (out) {
+          res.status(200);
+          res.json({
+            status: 200,
+            data : out
+          });
+      } else {
+        res.status(500)
+        res.json({
+            status: 500,
+            message : 'Oops, something unexpected happened. Blame Tarik Moustaid, I\'m just a program.'
+          });
+      }
+    });
+  }
+}
